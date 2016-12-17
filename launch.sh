65b7f244370b41d1c27c0708ea78c403a96e44ac
Hugo Posnic <hugo.posnic@protonmail.com>
2016-12-17 19:11:20 +0100

    Update launch script

--- launch.sh
+++ launch.sh
@@ -1,9 +1,10 @@
-# Use it just for local tests
+#!/bin/sh
+# Script to generate mo files in a locale folder
+# Use it just only for testing purpose
 rm -rf locale
 mkdir locale
 cd po
-for lang in $(ls -1 | sed -e 's/\..*$//')
-do
+for lang in $(ls -1 | sed -e 's/\..*$//'); do
     if [ $lang != "manjaro-hello" ]
     then
         mkdir -p ../locale/$lang/LC_MESSAGES
