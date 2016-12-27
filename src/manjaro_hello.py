6d7fef4e03b3160f1d8d2cd77559f03661d22ba1
Bernhard Landauer <oberon@manjaro.org>
2016-12-27 20:13:23 +0100

    add Dutch translations by DeMus

--- src/manjaro_hello.py
+++ src/manjaro_hello.py
@@ -78,7 +78,7 @@ class ManjaroHello():
             self.builder.get_object("stack").add_named(scrolled_window, page + "page")
 
         # Init translation
-        self.locales = ("de", "en", "es", "fr", "pl", "ru")  # supported locales
+        self.locales = ("de", "en", "es", "fr", "nl", "pl", "ru")  # supported locales
         self.default_locale = "en"
         self.sys_locale = locale.getdefaultlocale()[0]
         self.default_texts = {}
