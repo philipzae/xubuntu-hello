5c9b31a64c45ba0a01db3759610f0b9fe4f77516
Hugo Posnic <hugo.posnic@gmail.com>
2017-05-07 17:59:17 +0200

    Check if logo exists

--- src/manjaro_hello.py
+++ src/manjaro_hello.py
@@ -56,10 +56,11 @@ class ManjaroHello():
         self.builder.get_object("headerbar").props.subtitle = subtitle
 
         # Load logo
-        logo = GdkPixbuf.Pixbuf.new_from_file(self.logo_path)
-        self.window.set_icon(logo)
-        self.builder.get_object("manjaroicon").set_from_pixbuf(logo)
-        self.builder.get_object("aboutdialog").set_logo(logo)
+        if os.path.isfile(self.logo_path):
+            logo = GdkPixbuf.Pixbuf.new_from_file(self.logo_path)
+            self.window.set_icon(logo)
+            self.builder.get_object("manjaroicon").set_from_pixbuf(logo)
+            self.builder.get_object("aboutdialog").set_logo(logo)
 
         # Create pages
         self.pages = ("readme", "release", "involved")
