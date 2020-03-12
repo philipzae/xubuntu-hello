49d7839397c74035834cd437f530fc1536ff21c0
Yousuf 'Jay' Philips <philipz85@hotmail.com>
2020-03-12 23:10:20 +0400

    temporarily pausing the creation of locale folders
    Currently don't have any .po translation files so this causes the script to not run

--- launch.sh
+++ launch.sh
@@ -3,13 +3,13 @@
 # Use it only for testing purpose
 export PLUGIN_HELLO=True
 export PYTHONPATH="$HOME/test/gnome-layout-switcher/bin:$HOME/Data/projects/application-utility"
-rm -rf locale
-mkdir locale
-cd po
-for lang in $(ls *.po); do
-    lang=${lang::-3}
-    mkdir -p ../locale/${lang//_/-}/LC_MESSAGES
-    msgfmt -c -o ../locale/${lang//_/-}/LC_MESSAGES/manjaro-hello.mo $lang.po
-done
-cd ..
+#rm -rf locale
+#mkdir locale
+#cd po
+#for lang in $(ls *.po); do
+#    lang=${lang::-3}
+#    mkdir -p ../locale/${lang//_/-}/LC_MESSAGES
+#    msgfmt -c -o ../locale/${lang//_/-}/LC_MESSAGES/xubuntu-hello.mo $lang.po
+#done
+#cd ..
 python3 src/xubuntu_hello.py --dev
